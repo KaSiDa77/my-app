7884ff058176ec35879a65447bbef35f749924b3
KaSiDa77 <kabirsdayal@gmail.com>
2024-11-27 16:15:29 +0530

    Props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Cricketer extends React.Component{
+  constructor(props){
+    super(props)
+    this.player={name:"Rohit Kohli", age:34}
+  }
+  render(){
+    return <h1>The name of the player is {this.player.name} and he is {this.player.age} years old and has scored {this.props.buns.runs} in his life</h1>
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Runs extends React.Component{
+  render(){
+    const obj={runs:23233,name:"Sandulkar"}
+    return (<div>
+    <p>The 2nd most popular Sport:</p>
+    <Cricketer buns={obj}/>
+    </div>)
+  }
+}
+ReactDOM.render(<Runs/>, document.getElementById("root"))
